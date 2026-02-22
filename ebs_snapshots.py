e44f679ba52f6cc1ab307ed3a8bd94c47e3f9a2b
Bhoomika <bhoomikamr1997@gmail.com>
2026-02-22 19:14:04 +0530

    Snapshots cleanup updated

--- ebs_snapshots.py
+++ ebs_snapshots.py
@@ -8,11 +8,25 @@ ec2 = boto3.client('ec2')
 
 snapshots = ec2.describe_snapshots(OwnerIds=['self'])
 
-print(snapshots)
+#print(snapshots)
 
 for snapshot in snapshots.get('Snapshots',[]):
     if 'Created by CreateImage' in snapshot.get('Description',''):
         print(f"Snapshot {snapshot['SnapshotId']} cannot be deleted as it is linked to an AMI")
+    else:
+        try:
+            ec2.describe_volumes(VolumeIds=[snapshot['VolumeId']])
+            print(f"Snapshot {snapshot['SnapshotId']} cannot be deleted as its volume {snapshot['VolumeId']} still exists")
+        except Exception as e:
+            #print(f"Exception:{e}")
+            if 'InvalidVolume.NotFound' in str(e):
+                if dry_run:    
+                    print(f"DRY RUN will delete snapshot {snapshot['SnapshotId']} with unattached volume")
+                else:
+                    print(f"Deleting snapshot {snapshot['SnapshotId']}")
+                    ec2.delete_snapshot(SnapshotId=snapshot['SnapshotId'])
+                    print(f"Snapshot deleted successfully")
+
 
 
 
